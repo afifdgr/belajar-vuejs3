68e2133f25efd80898d78b0e543b495288de6777
afifdgr <afifdgr3@gmail.com>
2022-11-08 12:00:48 +0700

    #7 Components

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,19 @@
 <template>
-  <input type="text" v-model="cari" placeholder="Cari">
-
-  <ul>
-    <li v-for="product in cariData" :key="product.id">
-      {{ product.title }} - {{ product.price }}
-    </li>
-  </ul>
-
+  <Header />
 </template>
 
 <script>
-
+import Header from "@/components/Header.vue"
 
 export default {
   name: 'App',
-  data() {
-    return {
-      cari: "",
-      products: []
-    };
-  },
-  created() {
-    this.products = [
-      { id: 1, title: "Product 1", price: 3000 },
-      { id: 2, title: "Product 2", price: 5000 },
-      { id: 3, title: "Product 3", price: 7000 },
-      { id: 4, title: "Product 4", price: 1000 }
-    ];
-  },
-  computed: {
-    cariData() {
-      return this.products.filter((product) => {
-        return product.title.match(this.cari);
-      });
-    }
+  components: {
+    Header
   }
+
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
