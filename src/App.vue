cb000109adae6cd2ea281b572c248cf6655f977f
afifdgr <afifdgr3@gmail.com>
2022-11-08 11:13:04 +0700

    #4 Conditionals and Loops

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
-  <input type="text" v-model="nama">
-  {{ nama }}
+  <div v-if="products.length">
+    <ul>
+      <li v-for="product in products" :key="product.id">
+        {{ product.title }} - {{ product.price }}
+      </li>
+    </ul>
+  </div>
+  <div v-else>
+    <p>Data Not Found</p>
+  </div>
 </template>
 
 <script>
@@ -10,7 +18,24 @@ export default {
   name: 'App',
   data() {
     return {
-      nama: "Afiv"
+      products: [/* {
+        id: 1,
+        title: "Product 1",
+        price: 3000
+      }, {
+        id: 2,
+        title: "Product 2",
+        price: 5000
+      }, {
+        id: 3,
+        title: "Product 3",
+        price: 7000
+      }, {
+        id: 4,
+        title: "Product 4",
+        price: 1000
+      } */
+      ]
     };
   }
 }
