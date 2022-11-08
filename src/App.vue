2ec37072fb9dc5f99a9541596c318bba82cb60ea
afifdgr <afifdgr3@gmail.com>
2022-11-08 11:23:37 +0700

    #5 Lifecycle Hooks

--- src/App.vue
+++ src/App.vue
@@ -18,27 +18,27 @@ export default {
   name: 'App',
   data() {
     return {
-      products: [/* {
-        id: 1,
-        title: "Product 1",
-        price: 3000
-      }, {
-        id: 2,
-        title: "Product 2",
-        price: 5000
-      }, {
-        id: 3,
-        title: "Product 3",
-        price: 7000
-      }, {
-        id: 4,
-        title: "Product 4",
-        price: 1000
-      } */
-      ]
+      products: []
     };
-  }
-}
+  },
+  created() {
+    this.products = [
+      { id: 1, title: "Product 1", price: 3000 },
+      { id: 2, title: "Product 2", price: 5000 },
+      { id: 3, title: "Product 3", price: 7000 },
+      { id: 4, title: "Product 4", price: 1000 }
+    ];
+  },
+  /* beforeCreate() {
+    console.log('Before Create')
+  },
+  beforeMount() {
+    console.log('Before Mount')
+  },
+  mounted() {
+    console.log('Mounted')
+  } */
+};
 </script>
 
 <style>
